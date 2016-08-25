f118a02557ef82ce8e123502219e10f61b839338
Nicolas Simond <nicolassimond@posteo.ch>
2016-08-25 21:45:38 +0200

    Support for OpenSSL 1.1.0

--- build.sh
+++ build.sh
@@ -49,7 +49,7 @@ rm -rf openssl*
 
 #Download Latest nginx, nasxsi & OpenSSL, then extract.
 latest_nginx=$(curl -L http://nginx.org/en/download.html | egrep -o "nginx\-[0-9.]+\.tar[.a-z]*" | head -n 1)
-(curl -fLRO "https://openssl.org/source/openssl-1.1.0-pre6.tar.gz" && tar -xaf "openssl-1.1.0-pre6.tar.gz") &
+(curl -fLRO "https://www.openssl.org/source/openssl-1.1.0.tar.gz" && tar -xaf "openssl-1.1.0.tar.gz") &
 (curl -fLRO "http://nginx.org/download/${latest_nginx}" && tar -xaf "${latest_nginx}") &
 
 
