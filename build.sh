5f9e18afd6740643ce9739d3fbf58c762de0b7b4
novakin <guillaume.boyer@novakin.net>
2017-05-23 14:14:17 +0300

    Add support for Nginx headers more  module

--- build.sh
+++ build.sh
@@ -48,8 +48,8 @@ rm -rf openssl*
 #Download Latest nginx, nasxsi & OpenSSL, then extract.
 latest_nginx=$(curl -L http://nginx.org/en/download.html | egrep -o "nginx\-[0-9.]+\.tar[.a-z]*" | head -n 1)
 (curl -fLRO "https://www.openssl.org/source/openssl-1.1.0e.tar.gz" && tar -xaf "openssl-1.1.0e.tar.gz") &
-
 (curl -fLRO "http://nginx.org/download/${latest_nginx}" && tar -xaf "${latest_nginx}") &
+(curl -fLRO "https://github.com/openresty/headers-more-nginx-module/archive/v0.32.tar.gz" && tar -xaf "v0.32.tar.gz") &
 
 
 #Download Naxsi if wanted
@@ -103,6 +103,7 @@ $ngx_http2 \
 --with-threads \
 --with-http_ssl_module \
 --with-http_geoip_module \
+--add-module=../headers-more-nginx-module-0.32 \
 --with-openssl=/usr/src/${latest_openssl} \
 --with-ld-opt=-lrt \
 
