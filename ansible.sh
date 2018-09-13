0fda6deb2b6aa5c460b32b044b18186e30658064
Nicolas Simond <nicolassimond@posteo.ch>
2018-09-13 09:47:43 +0200

    Final TLS 1.3 support

--- ansible.sh
+++ ansible.sh
@@ -10,10 +10,10 @@ rm -rf openssl*
 
 #Download Latest nginx, nasxsi & OpenSSL, then extract.
 latest_nginx=$(curl -L http://nginx.org/en/download.html | egrep -o "nginx\-[0-9.]+\.tar[.a-z]*" | head -n 1)
-git clone https://github.com/openssl/openssl.git
+git clone https://github.com/openssl/openssl.git --branch OpenSSL_1_1_1-stable
 git clone https://github.com/hakasenyang/openssl-patch.git
 cd openssl
-patch -p1 < ../openssl-patch/openssl-equal-pre10_ciphers.patch
+patch -p1 < ../openssl-patch/openssl-equal-1.1.1_ciphers.patch
 cd /usr/src
 (curl -fLRO "http://nginx.org/download/${latest_nginx}" && tar -xaf "${latest_nginx}") &
 (curl -fLRO "https://github.com/openresty/headers-more-nginx-module/archive/v0.33.tar.gz" && tar -xaf "v0.33.tar.gz") &
